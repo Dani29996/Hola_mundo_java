a041c45cd8c13edd23ce7a5d7a0f45b228d9fc7e
poniente <poniente@ponienteDAM>
2017-03-06 12:08:09 +0100

    Primera subida del proyecto

---
+++ src/hola_mundo_java/Hola_Mundo_Java.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola_mundo_java;
+
+/**
+ *
+ * @author poniente
+ */
+public class Hola_Mundo_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("!Hola mundo¡");
+    }
+    
+}
